205f3e96e284b1a257e2b4a108a2dda9f84eae12
huzaifa1230 <huzaifairshad173@gmail.com>
2024-02-05 21:19:09 +0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
 
 export default function App() {
   return (
     <div>
-      <h1 className='text-red-400'>hihih</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
