5876911ec4584555b872b2151a39ee363ead6d9a
huzaifa1230 <huzaifairshad173@gmail.com>
2024-02-05 21:48:05 +0500

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
